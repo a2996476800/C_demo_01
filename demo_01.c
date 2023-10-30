71c82cce35b97b8dce64d78aa4e01a06cfde4ce2
启轩 尤 <2996476800@qq.com>
2023-10-30 21:41:24 +0800

    ?

--- demo_01.c
+++ demo_01.c
@@ -41,11 +41,81 @@
 //	printf("max = %d\n", max);
 //	return 0;
 //}
+//
+//
+//
+//int main()
+//{
+//	printf("%s\n""C:\\Project\\C\\demo_01.c");
+//	printf("\a\a\a\a\a\a\a");
+//	return 0;
+////}
+//
+//
+//int main()
+//{
+//	int input = 0;
+//	printf("要好好学习吗(0/1)?:");
+//	scanf("%d", &input);
+//	if (input == 1)
+//	{
+//		printf("拿offer\n");
+//	}
+//	else
+//	{
+//		printf("那就去卖红薯吧\n");
+//	}
+//	return 0;
+//}
 
+//int main()
+//{
+//	int line = 0;
+//	while (line < 30001)
+//	{
+//		printf("敲代码:%d\n", line);
+//		line++;
+//	}
+//	if (line == 30001);
+//	{
+//		printf("拿好offer\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+//用函数的方式解决求和
+
+int Add(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
+}
 
 
 int main()
 {
-	printf("C:\Project\C\demo_01.c");
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	int num4 = 0;
+	scanf("%d%d", &num1, &num2);
+	int sum1 = Add(num1, num2);
+	printf("sum1 = %d", sum1);
+	scanf("%d%d", &num3, &num4);
+	int sum2 = Add(num1, num2);
+	printf("sum2 = %d", sum2);
 	return 0;
 }
+
