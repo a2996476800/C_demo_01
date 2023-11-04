4a9efd01c2e944282b4137ea7a35460a4a4f23b3
启轩 尤 <2996476800@qq.com>
2023-11-04 23:17:57 +0800

    11/4

--- demo_01.c
+++ demo_01.c
@@ -586,16 +586,171 @@
 //}
 
 
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch > '9')
+//			continue;
+//
+//		putchar(ch);
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			break;
+//
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//}
+//不可在循环体内修改循环变量
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//int main(0)
+//{
+//	for (;;)
+//	{
+//		//判断部分的省略 - 判断部分恒为真 - 易死循环
+//		printf("hehe\n");
+//	}
+//
+//
+//	return 0;
+//})
+
+//
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+
+//计算n的阶乘
+//int main()
+//{ 
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	printf("%d\n", ret);
+//
+//	return 0;
+// }
+
+//计算1！+2！+……+10！
+//int main()
+//{ 
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 3; n++)
+//	{
+//		ret = 1;//计算n的阶乘之前，将ret的值初始化为1
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret *= i;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//
+//	return 0;
+// }
+//优化算法
+//int main()
+//{
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 3; n++)
+//	{
+//		ret *= n;
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+//在一个有序数组中查找具体的某个数字n
+//折半/二分查找法
 int main()
 {
-	int ch = 0;
-	while ((ch = getchar()) != EOF)
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+
+	int sz = sizeof(arr) / sizeof(arr[0]);//数组元素个数
+	int left = 0;
+	int right = sz - 1;
+	
+	while (left <= right)
 	{
-		if (ch < '0' || ch > '9')
-			continue;
-
-		putchar(ch);
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("已找到：%d\n", mid);
+			break;
+		}
 	}
-
+	if (left > right)
+	{
+		printf("找不到\n");
+	}
 	return 0;
-}
+ }
