154d88e84d0a9c9d6a1d079173cc65b70c0b7cb9
启轩 尤 <2996476800@qq.com>
2023-11-05 23:49:35 +0800

    11/5

--- demo_01.c
+++ demo_01.c
@@ -755,22 +755,49 @@
 //	return 0;
 // }
 
+//#include <windows.h>
+//int main()
+//{
+//	char arr1[] = "welcome to synu!!!!!";
+//	char arr2[] = "####################";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//
+//	while (left<=right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		Sleep(1000);//ÐÝÃßÒ»Ãë
+//		system("cls");//Çå¿ÕÆÁÄ»
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
 
 int main()
 {
-	char arr1[] = "welcome to synu!!!!!";
-	char arr2[] = "####################";
-	int left = 0;
-	int right = strlen(arr1) - 1;
-
-	while (left<=right)
+	int i = 0;
+	char password[20] = { 0 };
+	//¼ÙÉèÕýÈ·ÃÜÂëÎª×Ö·û´®"123456"
+	for (i = 0; i < 3; i++)
 	{
-		arr2[left] = arr1[left];
-		arr2[right] = arr1[right];
-		printf("%s\n", arr2);
-		left++;
-		right--;
+		printf("ÇëÊäÈëÃÜÂë£º");
+		scanf("%s", password);
+		if (strcmp(password, "123456") == 0)
+		{
+			printf("µÇÂ¼³É¹¦");
+		    break;
+		}
+		else {
+			printf("ÃÜÂë´íÎó\n");
+		}
 	}
-
+	if (i == 3)
+		printf("Èý´ÎÃÜÂë¾ù´íÎó£¬ÍË³ö³ÌÐò\n");
+	
 	return 0;
 }
